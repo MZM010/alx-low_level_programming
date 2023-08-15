4ddd6b087936f7fad381bd65fa79ac9577b02372
Mohamed zouine <mohamedzouine827@gmail.com>
2023-08-15 05:17:52 -0700

    shell

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * @a : first input
+ * @b : second input
+ * Description : add two numbers
+ * add - add two integer together
+ *
+ * Return : sum of a and b
+ *
+ * **/
+
+int add(int a, int b)
+{
+	int result;
+
+	result = a + b;
+	return (result);
+}
+
