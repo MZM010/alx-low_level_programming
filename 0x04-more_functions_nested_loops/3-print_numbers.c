eeb9e7cd011ddf01bc1f0e5b171ae83ec9b0ae4e
Mohamed zouine <mohamedzouine827@gmail.com>
2023-08-17 05:57:59 -0700

    shell

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include"main.h"
+
+/**
+ * print_numbers - print 0 - 9
+ *             only using _putchar twice
+ *
+ * Return: Always 0 (Success)
+*/
+
+void print_numbers(void)
+{
+	for (i = 0 + 48, i < = 0 + 57, i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
