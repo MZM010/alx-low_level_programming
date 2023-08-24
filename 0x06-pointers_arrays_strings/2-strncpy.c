d3d5852e447994a0f1157181ae311e45362ff8fb
Mohamed zouine <mohamedzouine827@gmail.com>
2023-08-24 10:07:27 -0700

    shell

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -21,7 +21,7 @@ char *_strncpy(char *dest, char *src, int n)
 	{
 		dest[i] = src[i];
 	}
-	for (i = j ; j < n ; j++)
-		dest[j] = '\0';
+	for (i = j ; i < n ; j++)
+		dest[i] = '\0';
 	return (dest);
 }
