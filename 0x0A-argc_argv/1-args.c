0dacd0eb5eff1d2650d25f10c5f6de99a0dde7cf
Mohamed zouine <mohamedzouine827@gmail.com>
2023-09-04 07:11:25 -0700

    shell

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-#include <stdlib.h>
 /**
  * main - a program that prints all arguments it receives
  *
@@ -9,11 +8,9 @@
  * Return: Always 0 (Success)
 */
 
-int main(int argc, char *argv[])
+int main(int argc, char *argv[] __attribute__((unused)))
 {
-	int i;
 
-	for (i = 0; i < argc; i++)
-		printf("%s\n", argv[i]);
+	printf("%d\n", argc - 1);
 	return (0);
 }
