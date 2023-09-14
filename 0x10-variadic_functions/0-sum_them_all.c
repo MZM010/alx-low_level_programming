c067cbf803784974b6c28abee0c9c749dc1dfefc
Mohamed zouine <mohamedzouine827@gmail.com>
2023-09-14 09:42:01 -0700

    shell

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -12,17 +12,15 @@ int sum_them_all(const unsigned int n, ...)
 {
 	int sum = 0;
 	unsigned int i;
-
-	if (n == 0)
-		return (0);
 	va_list supra;
 
 	va_start(supra, n);
 
 	for (i = 0; i < n; i++)
 	{
-		sum = sum + va_arg(supra, int);
+		sum +=  va_arg(supra, int);
 	}
 	va_end(supra);
+
 	return (sum);
 }
