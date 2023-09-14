3bd4a53e0fd03c983216718760dc60b777754e08
Mohamed zouine <mohamedzouine827@gmail.com>
2023-09-14 10:31:39 -0700

    print_numbers

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,22 @@
+#include "variadic_functions.c"
+
+/**
+ * print_numbers - prints numbers given as parameters
+ * @separator: string to be printed between numbers
+ * @n: number of integers
+ */
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	int i;
+	va_list pedro;
+
+	va_start(pedro, n);
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%d%c ", va_arg(pedro, int), separator);
+	}
+	va_end(pedro);
+	printf("\n");
+}
