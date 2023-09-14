1f5b5d5e33655c3bf7e734ae27bbbd8843c86075
Mohamed zouine <mohamedzouine827@gmail.com>
2023-09-14 09:27:09 -0700

    shell

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,28 @@
+#include "variadic_functions.h"
+
+/**
+ * sum_them_all - adds all its parameters
+ *
+ * @n: start of input variables
+ *
+ * Return: the sum
+*/
+
+int sum_them_all(const unsigned int n, ...)
+{
+	int sum = 0;
+	int i;
+
+	if (n == 0)
+		return (0);
+	va_list supra;
+
+	va_start(supra, n);
+
+	for (i = 0; i < sum; i++)
+	{
+		sum = sum + va_arg(supra, int);
+	}
+	va_end(supra);
+	return (sum);
+}
