9a6971f144defba5313cc08c0954bcc4b20a7f8b
Mohamed zouine <mohamedzouine827@gmail.com>
2023-12-07 19:04:34 +0100

    linked lists

--- 0x17-doubly_linked_lists/4-free_dlistint.c
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -2,12 +2,11 @@
 
 void free_dlistint(dlistint_t *head)
 {
-    dlistint_t *my_head = head;
     dlistint_t *alpha;
     while (head)
     {
-        alpha = my_head->next;
-        free(my_head);
-        my_head = alpha;
+        alpha = head->next;
+        free(head);
+        head = alpha;
     }
 }
