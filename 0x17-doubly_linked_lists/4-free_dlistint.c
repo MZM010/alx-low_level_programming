4abe40532fa961f96104acb4293bbc79d4e1aee4
Mohamed zouine <mohamedzouine827@gmail.com>
2023-12-07 19:10:08 +0100

    linked lists

--- 0x17-doubly_linked_lists/4-free_dlistint.c
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -1,12 +1,17 @@
 #include "lists.h"
+/**
+ * free_dlistint - return the number of element
+ * @head: the head
+ */
 
 void free_dlistint(dlistint_t *head)
 {
-    dlistint_t *alpha;
-    while (head)
-    {
-        alpha = head->next;
-        free(head);
-        head = alpha;
-    }
+	dlistint_t *alpha;
+
+	while (head)
+	{
+		alpha = head->next;
+		free(head);
+		head = alpha;
+	}
 }
